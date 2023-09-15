75a14e41ed29941068a259116355c483e282220f
CodeJM <jiaming45@hotmail.com>
2023-09-15 23:50:26 +0800

    commit

---
+++ Assets/Scripts/MovementControl.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementControl : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private Vector2 direction = Vector2.down;
+    public float speed = 5f;
+
+    public KeyCode inputUp = KeyCode.UpArrow;
+    public KeyCode inputDown = KeyCode.DownArrow;
+    public KeyCode inputLeft = KeyCode.LeftArrow;
+    public KeyCode inputRight = KeyCode.RightArrow;
+
+  [Header("Sprites")]
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    public AnimatedSpriteRenderer spriteRendererDeath;
+    private AnimatedSpriteRenderer firstSpriteRenderer;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        firstSpriteRenderer = spriteRendererDown;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(inputUp)) {
+            SetDirection(Vector2.up, spriteRendererUp);
+        } else if (Input.GetKey(inputDown)) {
+            SetDirection(Vector2.down, spriteRendererDown);
+        } else if (Input.GetKey(inputLeft)) {
+            SetDirection(Vector2.left, spriteRendererLeft);
+        } else if (Input.GetKey(inputRight)) {
+            SetDirection(Vector2.right, spriteRendererRight);
+        } else {
+            SetDirection(Vector2.zero, firstSpriteRenderer);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position + translation);
+    }
+
+    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
+    {
+        direction = newDirection;
+
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        firstSpriteRenderer = spriteRenderer;
+        firstSpriteRenderer.idle = direction == Vector2.zero;
+    }
+}
